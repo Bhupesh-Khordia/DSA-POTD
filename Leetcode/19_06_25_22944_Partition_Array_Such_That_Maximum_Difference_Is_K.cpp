c465421e94e96b0914c4653e73106d37eb53f372
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-06-19 16:11:57 +0530

    Create 19_06_25_22944_Partition_Array_Such_That_Maximum_Difference_Is_K.cpp

---
+++ Leetcode/19_06_25_22944_Partition_Array_Such_That_Maximum_Difference_Is_K.cpp
@@ -0,0 +1,19 @@
+// https://leetcode.com/problems/partition-array-such-that-maximum-difference-is-k/?envType=daily-question&envId=2025-06-19
+
+class Solution {
+public:
+    int partitionArray(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+
+        int mini = nums[0];
+        int ans = 1;
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] - mini > k) {
+                ans++;
+                mini = nums[i];
+            }
+        }
+
+        return ans;
+    }
+};

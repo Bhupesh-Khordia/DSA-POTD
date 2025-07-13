033f230599939c0bc006f5b57dd33487bb7cae1c
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-07-13 07:40:57 +0530

    Create 13_07_25_2410_Maximum_Matching_of_Players_With_Trainers.cpp

---
+++ Leetcode/13_07_25_2410_Maximum_Matching_of_Players_With_Trainers.cpp
@@ -0,0 +1,21 @@
+// https://leetcode.com/problems/maximum-matching-of-players-with-trainers/description/?envType=daily-question&envId=2025-07-13
+
+class Solution {
+public:
+    int matchPlayersAndTrainers(vector<int>& players, vector<int>& trainers) {
+        sort(players.begin(), players.end());
+        sort(trainers.begin(), trainers.end());
+
+        int i = 0, j = 0, ans = 0;
+        while(i < players.size() && j < trainers.size()) {
+            if(players[i] <= trainers[j]) {
+                ans++;
+                i++;
+                j++;
+            } else {
+                j++;
+            }
+        }
+        return ans;
+    }
+};

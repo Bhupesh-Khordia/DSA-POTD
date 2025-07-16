8ac11814e72cc8d11fe6d8a5730939227a2e2857
Bhupesh-Khordia <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-07-16 05:45:58 +0530

    Create 16_07_25_3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.cpp

---
+++ Leetcode/16_07_25_3201_Find_the_Maximum_Length_of_Valid_Subsequence_I.cpp
@@ -0,0 +1,20 @@
+// https://leetcode.com/problems/find-the-maximum-length-of-valid-subsequence-i/description/?envType=daily-question&envId=2025-07-16
+
+class Solution {
+public:
+    int maximumLength(vector<int>& nums) {
+        int c = nums[0] % 2, odd = 0, even = 0, both = 0;
+        for (int num : nums) {
+            if (num % 2 == 0) {
+                even++;
+            } else {
+                odd++;
+            }
+            if (num % 2 == c) {
+                both++;
+                c = 1 - c;  // Toggle the parity
+            }
+        }
+        return max(both, max(even, odd));
+    }
+};

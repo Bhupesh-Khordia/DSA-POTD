b0f6b63a6836ad75330e4f2ae47f054d96d2fbea
Bhupesh Yadav <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-08-14 19:22:20 +0530

    Create 14_08_25.cpp

---
+++ Leetcode/14_08_25.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    string largestGoodInteger(string num) {
+        char ans = '\0';
+        for(int i = 1; i < num.length() - 1; i++) {
+            if(num[i] == num[i - 1] && num[i] == num[i + 1]) {
+                ans = max(ans, num[i]);
+            }
+        }
+        return ans ? string(3, ans) : "";
+    }
+};

d4dc97824bcd7cb1373955318e74177730b27102
Bhupesh Yadav <143842550+Bhupesh-Khordia@users.noreply.github.com>
2025-10-20 18:41:50 +0530

    Add finalValueAfterOperations method in Solution class
    Implement finalValueAfterOperations to calculate final value of x based on operations.

---
+++ Leetcode/20_10_25.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int finalValueAfterOperations(vector<string>& operations) {
+        int n =  operations.size();
+        int x = 0;
+
+        for(int i =0;i<n;i++){
+            if(operations[i] == "X++" || operations[i] == "++X"){
+                x+=1;
+            }
+            if(operations[i] == "X--" || operations[i] == "--X"){
+                x-=1;
+            }
+
+        }
+        return x;
+        
+    }
+};
